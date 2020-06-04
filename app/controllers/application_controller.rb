6f544889b7d67bf3cbf3ef89e6e396c8ace2bd3f
USER <k.k.fliper12@gmail.com>
2020-06-04 15:54:10 +0900

    login 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :url])
-    logger.debug("if文の中に入りました")
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :password])
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,10 @@
 class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable, :trackable, :confirmable
+          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :properties
+
 end
