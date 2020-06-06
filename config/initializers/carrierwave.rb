3d0e73055047fb604cf5268af894c7eddc5a5e0f
USER <k.k.fliper12@gmail.com>
2020-06-06 17:18:34 +0900

    access_key secret_key

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id:     ENV['ACCESS_KEY'],
+    aws_secret_access_key: ENV['SECRET_KYE'],
+    region: 'ap-northeast-1'
+  }
+
+  config.storage = :fog
+  config.permissions = 0666
+  config.cache_dir = "#{Rails.root}/tmp/"
+  config.fog_directory  = ENV['FOG_DIRECTORY']
+  config.fog_attributes = {'Cashe-Control' => "max-age=#{365.day.to_i}"}
+end
