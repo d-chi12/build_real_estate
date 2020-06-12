7884249b6b5d64302a77d9efe2a15b137b818471
USER <k.k.fliper12@gmail.com>
2020-06-12 17:44:50 +0900

    account list レイアウト

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -5,6 +5,10 @@ module PropertiesHelper
     image_tag img, class: "property-thumb"
   end
 
+  def property_thumbnail_url property
+    property.photo.present? ? property.photo.thumb.url : "noimage.png"
+  end
+
   def property_photo_url property
     property.photo.present? ? property.photo.url : asset_url("noimage.png")
   end
