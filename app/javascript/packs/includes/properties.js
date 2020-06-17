d5d3acdd61a3db0951e08c984056cbfd71545755
USER <k.k.fliper12@gmail.com>
2020-06-17 11:08:43 +0900

    send message

--- app/javascript/packs/includes/properties.js
+++ app/javascript/packs/includes/properties.js
@@ -14,10 +14,34 @@ $(function(){
     }
   });
 
+  $("#send-message-to-agent").on("click", function(){
+    var agent_id = $("#agent_id").val(),
+        first_name = $("#message-first-name").val(),
+        last_name = $("#message-last-name").val(),
+        email = $("#message-email").val()
+        message = $("#message-text").val();
+
+    $.ajax({
+      url: "/agent/message",
+      method: "POST",
+      dataType: "json",
+      data: {
+        agent_id: agent_id,
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        message: message
+      },
+      success: function(data){
+        $('#contact-model form').remove();
+        $('#send-message-to-agent').remove();
+        $('#contact-model .modal-body').html("<p>Your message has been sent successfully!</p>");
+      }
+    })
+  });
+
   $('#contact-modal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) 
     var modal = $(this)
-  
   })
-
 });
