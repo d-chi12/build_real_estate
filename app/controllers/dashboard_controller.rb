dde9096159b63fc06a1a6f479f7fc46a1a1b540b
USER <k.k.fliper12@gmail.com>
2020-06-23 23:47:39 +0900

    修正

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,7 @@ class DashboardController < ApplicationController
 
   def profile
     @account = Account.find(params[:id])
-    @agent_properties = Property.where(account_id: @account.id)
+    @properties = Property.where(account_id: @account.id)
 
     @properties_sold = Property.where(account_id: @account.id).sold.count
     @properties_for_sale = Property.where(account_id: @account.id).for_sale.count
