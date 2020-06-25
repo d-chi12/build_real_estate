7007ecc8be4459db05de0061ed4a2ee9247f721c
USER <k.k.fliper12@gmail.com>
2020-06-25 15:32:32 +0900

    modify jsfile

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
-// const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker')
 
-// const webpack = repuire('webpack')
-// environment.plugins.prepend('Provide',
-//   new webpack.ProvidePlugin({
-//       $: 'jquery/src/jquery',
-//       jQuery: 'jquery/src/jquery',
-//       popper: ['popper.js', 'default']
-//   }) 
-//   )
-//   module.exports = environment
+const webpack = repuire('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+      $: 'jquery/src/jquery',
+      jQuery: 'jquery/src/jquery',
+      popper: ['popper.js', 'default']
+  }) 
+  )
+  module.exports = environment
